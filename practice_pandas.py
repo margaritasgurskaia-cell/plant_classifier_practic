1918602c67921e009fb172180d8da1df06d091b3
Margaret Gurskaya <margarita.s.gurskaia@gmail.com>
2026-07-29 12:03:52 +0300

    Practice modul 5, lesson 6

---
+++ practice_pandas.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+plants = pd.DataFrame({
+    "name": ["catalpa", "lavender", "willow"],
+    "leaf_count": [10000, 500, 100000],
+    "color": ["green","violet", "pale green"]
+})
+
+print(plants)
